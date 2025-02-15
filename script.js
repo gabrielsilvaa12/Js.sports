6458fd5143ebdfa5c4ae6c50974e267c727bb7b3
Gabriel Aparecido <gabrieusilvaa14@gmail.com>
2025-02-15 16:38:18 -0300

    Add files via upload
    DETAILS: 
    - Arrumar imagens no "index.html"
    - Fazer a tela de agenda

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-document.getElementById("menu-btn").addEventListener("click", function() {
+document.getElementById("menu-btn").addEventListener("click", function () {
     let menu = document.getElementById("menu");
     menu.style.display = menu.style.display === "block" ? "none" : "block";
 });
 
 
-document.addEventListener("click", function(event) {
+document.addEventListener("click", function (event) {
     let menu = document.getElementById("menu");
     let button = document.getElementById("menu-btn");
     if (!menu.contains(event.target) && !button.contains(event.target)) {
@@ -18,8 +18,8 @@ function moveSlide(direction) {
     const slides = document.querySelector(".carrossel-container");
     index += direction;
 
-    if (index > 1) index = 0;  
-    if (index < 0) index = 1;  
+    if (index > 1) index = 0;
+    if (index < 0) index = 1;
 
     slides.style.transform = `translateX(${-index * 1100}px)`;
 }
